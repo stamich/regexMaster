ef0b7902a981f26307ec2c314af6cd670fae7310
Michal Stawarski <m.ski@poczta.fm>
2019-04-11 15:05:17 +0200

    init commit

---
+++ LogHandler.cpp
@@ -0,0 +1,50 @@
+//
+// Created by Michal S. on 2019-04-11.
+//
+
+#include <string>
+#include <ctime>
+#include <list>
+#include "LogHandler.h"
+
+using namespace std;
+
+LogHandler::LogHandler() {
+
+}
+
+list<string> LogHandler::loadLogFile(string absolutePath) {
+
+}
+
+void LogHandler::makeLogHeader() {
+
+}
+
+void LogHandler::makeLogFooter() {
+
+}
+
+void LogHandler::searchLastFiles(string fileName) {
+
+}
+
+void LogHandler::makeInfoMessage(string fileName) {
+
+}
+
+void LogHandler::makeWarningMessage(string fileName) {
+
+}
+
+void LogHandler::makeEmptyMessage(string fileName) {
+
+}
+
+void LogHandler::makeErrorMessage(string fileName) {
+
+}
+
+LogHandler::~LogHandler() {
+
+}
---
+++ LogHandler.h
@@ -0,0 +1,35 @@
+//
+// Created by Michal S. on 2019-04-11.
+//
+
+#ifndef REGEXMASTER_LOGHANDLER_H
+#define REGEXMASTER_LOGHANDLER_H
+
+#include <string>
+#include <ctime>
+#include <list>
+
+using namespace std;
+
+class LogHandler {
+
+    string line;
+    string newFile;
+    struct tm* today;
+    struct tm* formatter;
+    string logFilePath;
+
+public:
+    LogHandler();
+    list<string> loadLogFile(string absolutePath);
+    void makeLogHeader();
+    void makeLogFooter();
+    void searchLastFiles(string fileName);
+    void makeInfoMessage(string fileName);
+    void makeWarningMessage(string fileName);
+    void makeEmptyMessage(string fileName);
+    void makeErrorMessage(string fileName);
+    ~LogHandler();
+};
+
+#endif //REGEXMASTER_LOGHANDLER_H
